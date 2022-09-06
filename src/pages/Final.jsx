01e47570ba00c7965b5931786d1eb11e4535d6c7
dvdgrbvc <david_grabovac@hotmail.de>
2022-09-06 12:07:52 +0200

    Correction

--- src/pages/Final.jsx
+++ src/pages/Final.jsx
@@ -5,7 +5,7 @@ const Final = () => {
   return (
     <div class="background">
       <Navbar/>
-        <p class="text-center text-6xl text-white pt-32 pb-20">Describe what happended and send an email to :</p>
+        <p class="text-center text-6xl text-white pt-32 pb-20">Describe what happened and send an email to :</p>
         <a class="block text-center text-[64px] text-[#0066CC] font-bold font-['Inter']" href="mailto:david.ojstersek@ruby-hotels.com">david.ojstersek@ruby-hotels.com</a>
     </div>
   )
